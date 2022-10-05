a0f53bc718a05a8852f2ae0f496f1354081a9cb3
amonchaiz <amonchai.p@zeabix.com>
2022-10-05 09:55:43 +0700

    Add Readme

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Ananda IAC
+![Terraform](https://github.com/zeabix-cloud-native/ananda-iac/actions/workflows/terraform.yaml/badge.svg)
+
+## Requirements
+- [Terraform](https://www.terraform.io/downloads.html)
+
+
+## Usage
+```sh
+terraform init
+terraform plan
+terraform apply
+```
+
+## Destroy Resource
+Once `terraform apply` has successfully completed, to destroy the all resource run these commands
+```sh
+terraform destroy
+```
--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_container_registry" "acr" {
 #### AKS #####
 resource "azurerm_kubernetes_cluster" "cluster" {
   kubernetes_version  = var.cluster_version
-  name                = "${var.cluster_name}-cluster"
+  name                = "${var.anotation_name}-cluster"
   location            = azurerm_resource_group.group.location
   resource_group_name = azurerm_resource_group.group.name
   dns_prefix           = var.dns_prefix
