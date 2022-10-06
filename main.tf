b7608ebc751c2840deb3ba87096b693c1cb8666f
amonchaiz <amonchai.p@zeabix.com>
2022-10-06 15:54:41 +0700

    Add APIM diagnostic

--- main.tf
+++ main.tf
@@ -16,6 +16,69 @@ resource "azurerm_api_management" "apim" {
 
   sku_name = var.apim_tier
 }
+resource "azurerm_application_insights" "insights" {
+  name                = "${var.anotation_name}-appinsights"
+  location            = azurerm_resource_group.group.location
+  resource_group_name = azurerm_resource_group.group.name
+  application_type    = "web"
+}
+resource "azurerm_api_management_logger" "logger" {
+  name                = "${var.anotation_name}-apimlogger"
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.group.name
+
+  application_insights {
+    instrumentation_key = azurerm_application_insights.insights.instrumentation_key
+  }
+}
+resource "azurerm_api_management_diagnostic" "diagnostic" {
+  identifier               = "${var.anotation_name}-applicationinsights"
+  resource_group_name      = azurerm_resource_group.group.name
+  api_management_name      = azurerm_api_management.apim.name
+  api_management_logger_id = azurerm_api_management_logger.logger.id
+
+  sampling_percentage       = 5.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "verbose"
+  http_correlation_protocol = "W3C"
+
+  frontend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  frontend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+
+  backend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  backend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+}
 ### END APIM ###
 ### Cert mTLS ###
 resource "azurerm_api_management_certificate" "tls" {
